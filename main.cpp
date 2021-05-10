57b15232553e15d7ad44fa8ccb92738c91650954
Abhishek Jha <abhi.jha.cs@gmail.com>
2021-05-10 20:20:13 +0530

    uploaded cp setup

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define cin std::cin
+#define cout std::cout
+#define endl "\n"
+#define mod 1000000007
+#define pi 3.141592653589793238462643
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define ff first
+#define ss second
+#define rep(i,n) for(int i=0; i<n; ++i)
+#define round(n,i) fixed<<setprecision(i)<<n
+
+void solve(){
+    
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL); cout.tie(NULL);
+	ll t; 
+	cin>>t; 
+	for(ll i=1; i<=t; ++i){
+        solve();
+    }
+}
